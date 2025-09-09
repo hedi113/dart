475d25d6a73fca451d633bed1a70350e43e455be
hedi113 <114721882+hedi113@users.noreply.github.com>
2025-09-09 11:18:06 +0200

    Add files via upload

---
+++ dart1.dart
@@ -0,0 +1,15 @@
+void main() {
+    print("Szegedi SZC Vasvári Pál Gazdasági és Informatikai Technikum");
+
+    String utca = "Gutenberg utca";
+    String telepules = "Szeged";
+    int hazszam = 11;
+    int iranyitoszam = 6722;
+
+    print("Szegedi SZC Vasvári Pál Gazdasági és Informatikai Technikum, $utca $hazszam, $telepules $iranyitoszam");
+
+    int kilencedik = 28 * 4;
+    int tobbi = 3 * 28;
+
+    print("Kilencedik évfolyam létszáma: $kilencedik \nTizedik évfolyam létszáma: $tobbi\nTizenegyedik évfolyam létszáma: $tobbi \nTizenkettedik évfolyam létszáma: $tobbi \nTizenharmadik évfolyam létszáma: $tobbi"); 
+} 
